450d7e51292f419e25579d91274f699765e3b32b
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-10-04 03:05:29 +0600

    btn2 click function add

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ function btn1() {
     const total_price = document.getElementById('total_price')
     const total_priceparse = parseFloat(total_price.innerText)
 
-    const price = document.getElementById('price')
-    const priceparse = parseFloat(price.innerText)
+    const price1 = document.getElementById('price1')
+    const priceparse = parseFloat(price1.innerText)
 
     const Model1=document.getElementById('Model1')
     const phnname = Model1.innerText;
@@ -23,5 +23,32 @@ function btn1() {
    `
    cartlist.appendChild(li)
    
+}
 
+
+function btn2() {
+    const total_price = document.getElementById('total_price')
+    const total_priceparse = parseFloat(total_price.innerText)
+
+    const price2 = document.getElementById('price2')
+    const priceparse = parseFloat(price2.innerText)
+
+    const Model2=document.getElementById('Model2')
+    const phnname = Model2.innerText;
+
+    total_price.innerText = priceparse + total_priceparse
+
+    const cartlist = document.getElementById('list')
+    const li = document.createElement('li')
+    li.innerHTML = `
+   <div id="card" class="w-full flex gap-5 items-center  border-2 border-black p-2 bg-slate-300 rounded mb-2">
+   <aside>
+       <img class="w-20" src="img/12pro-removebg-preview.png" alt="image">
+   </aside>
+   <h2 class="text-lg font-semibold text-black capitalize"> ${phnname}</h2>
+   <h2 class="card-title ">Price $ <span class="text-red-500">${priceparse}</span></h2>
+    </div>
+   `
+   cartlist.appendChild(li)
+   
 }
