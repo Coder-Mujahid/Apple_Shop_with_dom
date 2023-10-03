9f494afb9c3e2960708a04b5dab0dd23f0af9fa7
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-10-04 03:09:39 +0600

    btn3 click function add

--- app.js
+++ app.js
@@ -52,3 +52,31 @@ function btn2() {
    cartlist.appendChild(li)
    
 }
+
+
+function btn3() {
+    const total_price = document.getElementById('total_price')
+    const total_priceparse = parseFloat(total_price.innerText)
+
+    const price3 = document.getElementById('price3')
+    const priceparse = parseFloat(price3.innerText)
+
+    const Model3=document.getElementById('Model3')
+    const phnname = Model3.innerText;
+
+    total_price.innerText = priceparse + total_priceparse
+
+    const cartlist = document.getElementById('list')
+    const li = document.createElement('li')
+    li.innerHTML = `
+   <div id="card" class="w-full flex gap-5 items-center  border-2 border-black p-2 bg-slate-300 rounded mb-2">
+   <aside>
+       <img class="w-20" src="img/12pro-removebg-preview.png" alt="image">
+   </aside>
+   <h2 class="text-lg font-semibold text-black capitalize"> ${phnname}</h2>
+   <h2 class="card-title ">Price $ <span class="text-red-500">${priceparse}</span></h2>
+    </div>
+   `
+   cartlist.appendChild(li)
+   
+}
